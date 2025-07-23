6227cad93961ea345056e71987f45f689713f915
fbwougr121 <fbwogur121@naver.com>
2025-07-23 12:23:56 +0900

    SHINSEGAE Java 9th education first commit

---
+++ src/ex01.java
@@ -0,0 +1,13 @@
+public class ex01 {
+    public static void main(String[] args) {
+        //3개의 정수 10, 11, 12를 더하여 결과를 출력하시오.
+        int a = 10;
+        int b = 11;
+        int c = 12;
+        int sum = a+b+c;
+        System.out.printf("res: %d", a+b+c);
+        System.out.println();
+        System.out.println();  // cmd + d -> copy line
+        System.out.println("res: " + sum);
+    }
+}
