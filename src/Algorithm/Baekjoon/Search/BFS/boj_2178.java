c005e77ad10f5e19706cecb2ab06d7a928ef0554
fbwougr121 <fbwogur121@naver.com>
2025-07-31 12:45:05 +0900

    DoubleArray Ex01

---
+++ src/Algorithm/Baekjoon/Search/BFS/boj_2178.java
@@ -0,0 +1,17 @@
+package Algorithm.Baekjoon.Search.BFS;
+
+import java.io.IOException;
+
+public class boj_2178 {
+    static int n,m;
+    static int[][] arr;
+    static boolean[][] visited;
+
+
+    public static void bfs(int x, int y){
+
+    }
+    public static void main(String[] args) throws IOException {
+
+    }
+}
