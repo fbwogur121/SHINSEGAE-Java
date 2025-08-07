af0e5eea238d2a7e89eb192424d03740fefe388c
fbwougr121 <fbwogur121@naver.com>
2025-08-07 15:41:17 +0900

    Inheritance_Parametic Polymorphism

--- src/august/aug_0807/polyMorphism/example02/Taxi.java
+++ src/august/aug_0807/polyMorphism/example02/Taxi.java
@@ -1,7 +1,10 @@
 package august.aug_0807.polyMorphism.example02;
 
-public class Taxi  {
+public class Taxi extends Vehicle {
     //메소드 재정의(오버라이딩)
-
+    @Override
+    public void run(){
+        System.out.println("택시가 달립니다.");
+    }
 
 }
